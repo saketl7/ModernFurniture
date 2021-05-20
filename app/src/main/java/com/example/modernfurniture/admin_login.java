8763f48ebbfd44b41e8d5877419deef6d58c27c1
saketlakhotia <saket.lakhotia17>
2021-05-20 12:15:10 +0530

    Added admin page

--- app/src/main/java/com/example/modernfurniture/admin_login.java
+++ app/src/main/java/com/example/modernfurniture/admin_login.java
@@ -2,25 +2,17 @@ package com.example.modernfurniture;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class admin_login extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    Button btn;
-    EditText emailAddress, password1;
+    private EditText email, pass;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,34 +21,24 @@ public class admin_login extends AppCompatActivity {
 
 
         btn = findViewById(R.id.btnlogin2);
-        emailAddress = findViewById(R.id.email);
-        password1 = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
 
 
-
-        mAuth = FirebaseAuth.getInstance();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                validate(email.getText().toString(), pass.getText().toString());
+            }
 
-                String email = emailAddress.getText().toString().trim();
-                String Pass = password1.getText().toString().trim();
-
-                if(!TextUtils.isEmpty(email)&&!TextUtils.isEmpty(Pass)){
-                    mAuth.signInWithEmailAndPassword(email,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                startActivity(new Intent(admin_login.this,Add_products.class));
-                                finish();
-                            }else {
-                                Toast.makeText(admin_login.this,"Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }else {
-                    Toast.makeText(admin_login.this,"You forget to put some data",Toast.LENGTH_SHORT).show();
+            public void validate(String inputEmail, String inputUsername) {
+                if((inputEmail.equals("admin@gmail.com"))&&(inputUsername.equals("12345"))){
+                    Intent intent = new Intent(admin_login.this, Add_products.class);
+                    startActivity(intent);
+                    Toast.makeText(admin_login.this, "You have successfully logged in", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(admin_login.this,"Please enter correct credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
