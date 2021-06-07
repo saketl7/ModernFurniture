cb8c5f04a17c7a9ac6905474e5433defdfafb190
Aena02 <84123094+Aena02@users.noreply.github.com>
2021-06-07 11:55:01 +0530

    Add files via upload

--- app/src/main/java/com/example/modernfurniture/Login.java
+++ app/src/main/java/com/example/modernfurniture/Login.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +38,7 @@ public class Login extends AppCompatActivity {
 
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.pass);
-        progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar2);
         fAuth = FirebaseAuth.getInstance();
         mLoginBtn = findViewById(R.id.btnlogin);
         mCreateBtn = findViewById(R.id.textViewSignUp);
@@ -54,7 +55,7 @@ public class Login extends AppCompatActivity {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
+                if(TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
                     mEmail.setError("Email is Required.");
                     return;
                 }
@@ -70,6 +71,8 @@ public class Login extends AppCompatActivity {
                 }
 
 
+                progressBar.setVisibility(View.VISIBLE);
+
 
                 // authenticate the user
 
@@ -81,7 +84,7 @@ public class Login extends AppCompatActivity {
                             startActivity(new Intent(getApplicationContext(),gallery.class));
                         }else {
                             Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
+                            progressBar.setVisibility(View.INVISIBLE);
                         }
 
                     }
